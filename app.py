8109340ebe7a8d47aa08bac7091cf9db9f26929f
Tim Brodsky <tetbetwork@gmail.com>
2024-04-03 11:48:16 +0300

    Initial setup of the Add route

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,14 @@ def index():
     with open('blog_data.json', 'r') as f:
         blog_posts = json.load(f)
     return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        # we will fill this in the next step
+        pass
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
