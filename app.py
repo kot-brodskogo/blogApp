6624c2821622ecb83a72f3dd14b8ad7adc88a5ae
Tim Brodsky <tetbetwork@gmail.com>
2024-04-03 13:31:31 +0300

    Delete option was added

--- app.py
+++ app.py
@@ -5,11 +5,35 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
+class BlogManager:
+    def __init__(self, data_file):
+        self.data_file = data_file
+        self.blog_posts = []  # Initialize blog_posts as an empty list
+        self.load_posts()
+
+    def load_posts(self):
+        with open(self.data_file, 'r') as f:
+            self.blog_posts = json.load(f)
+
+    def save_posts(self):
+        with open(self.data_file, 'w') as f:
+            json.dump(self.blog_posts, f, indent=4)
+
+    def add_post(self, new_post):
+        self.blog_posts.append(new_post)
+        self.save_posts()
+
+    def delete_post(self, post_id):
+        self.blog_posts = [post for post in self.blog_posts if str(post['id']) != post_id]
+        self.save_posts()
+
+
+blog_manager = BlogManager('blog_data.json')
+
+
 @app.route('/')
 def index():
-    with open('blog_data.json', 'r') as f:
-        blog_posts = json.load(f)
-    return render_template('index.html', posts=blog_posts)
+    return render_template('index.html', posts=blog_manager.blog_posts)
 
 
 @app.route('/add', methods=['GET', 'POST'])
@@ -31,20 +55,18 @@ def add():
             'content': content
         }
 
-        # Read existing blog posts from the JSON file
-        with open('blog_data.json', 'r') as f:
-            blog_posts = json.load(f)
-
-        # Append the new blog post to the list of existing blog posts
-        blog_posts.append(new_post)
-
-        # Write the updated list of blog posts back to the JSON file
-        with open('blog_data.json', 'w') as f:
-            json.dump(blog_posts, f, indent=4)
+        # Add the new blog post using the BlogManager instance
+        blog_manager.add_post(new_post)
 
         # Redirect to the index page after adding the new blog post
         return redirect(url_for('index'))
     return render_template('add.html')
+
+
+@app.route('/delete/<post_id>', methods=['POST'])
+def delete(post_id):
+    blog_manager.delete_post(post_id)
+    return redirect(url_for('index'))
 
 
 def generate_unique_id():
@@ -53,4 +75,4 @@ def generate_unique_id():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
